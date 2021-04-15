dceed1acee8f4c33562f193b3d56e89484cc2f48
dhiagara <dhia-gara@hotmail.fr>
2021-04-16 00:25:39 +0200

    first commit

---
+++ daddesh/demo/dao/entity/Produit.java
@@ -0,0 +1,23 @@
+package daddesh.demo.dao.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nom;
+    private String category;
+    private String type;
+}
